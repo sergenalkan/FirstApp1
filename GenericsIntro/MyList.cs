6c591b6eb4a6a69c3179471d88507b9fc6db4797
Sergen Alkan <sergen_alkan@hotmail.com>
2023-01-02 23:43:07 +0300

    02.01.2023 Ders Sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor
+        //ctor
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length+1];
+        }
+
+
+    }
+}
